9de712c376deec9de99283385402152144ef06d8
artxnv <artxnv@gmail.com>
2024-10-05 22:59:22 +0200

    step 2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,19 +1,36 @@
 import argparse
 
+
 def main():
     # Создание парсера аргументов
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
 
     # Определение позиционных аргументов
-    parser.add_argument('first_file', help='Path to the first configuration file')
-    parser.add_argument('second_file', help='Path to the second configuration file')
+    parser.add_argument(
+        'first_file',
+        help='Path to the first configuration file'
+    )
+    parser.add_argument(
+        'second_file',
+        help='Path to the second configuration file'
+    )
+
+    # Определение опции --format
+    parser.add_argument(
+        '-f', '--format',
+        help='Set format of output (plain or json)',
+        default='plain'
+    )
 
     # Обработка аргументов
     args = parser.parse_args()
 
     # Здесь можно добавить логику сравнения файлов
-    # Например, просто выводим полученные аргументы
     print(f'Comparing files: {args.first_file} and {args.second_file}')
+    print(f'Output format: {args.format}')
+
 
 if __name__ == '__main__':
     main()
